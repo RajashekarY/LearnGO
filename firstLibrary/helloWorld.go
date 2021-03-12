6dd7a4c9ee2b860c5df284fdb6b84f6801795120
Rajashekar Y <yadavallirajashekar@gmail.com>
2021-03-12 10:29:35 +0530

    first library testing

---
+++ firstLibrary/helloWorld.go
@@ -0,0 +1,10 @@
+package hello
+
+import (
+	"fmt"
+	"time"
+)
+
+func helloWorld() {
+	fmt.Printf("Hello world this program is a library package written by rajashekar on %s %s", time.Hour, time.Local)
+}
